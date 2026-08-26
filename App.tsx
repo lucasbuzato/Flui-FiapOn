8653a0c4327b704a92c358e92be6cc796c89bd61
Lucas <sergiorockemroll@gmail.com>
2026-08-25 22:00:42 -0300

    Primeira Tela Feita

---
+++ App.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react';
+import { useFonts } from 'expo-font';
+import {
+  Karla_200ExtraLight,
+  Karla_300Light,
+  Karla_400Regular,
+  Karla_500Medium,
+  Karla_600SemiBold,
+  Karla_700Bold,
+  Karla_800ExtraBold,
+} from '@expo-google-fonts/karla';
+import * as SplashScreen from 'expo-splash-screen';
+import HomeScreen from './src/screens/HomeScreen';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    Karla_200ExtraLight,
+    Karla_300Light,
+    Karla_400Regular,
+    Karla_500Medium,
+    Karla_600SemiBold,
+    Karla_700Bold,
+    Karla_800ExtraBold,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  return <HomeScreen />;
+}
